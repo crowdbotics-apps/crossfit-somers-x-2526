a6dc33596d753f05201de0cb15555a47f3e1b326
Crowdbotics <team@crowdbotics.com>
2019-04-21 22:08:35 +0000

    initial commit

---
+++ android/app/src/main/java/com/crossfit_somers_x_2526/MainActivity.java
@@ -0,0 +1,15 @@
+package com.crossfit_somers_x_2526;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "crossfit_somers_x_2526";
+    }
+}
